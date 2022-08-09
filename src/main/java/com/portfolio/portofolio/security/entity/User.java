9061fd8fddc0776004729aeb787a795228dad234
Agustin Boccio <aboccio@hotmail.com>
2022-08-09 18:40:42 +0200

    User class

--- src/main/java/com/portfolio/portofolio/security/entity/User.java
+++ src/main/java/com/portfolio/portofolio/security/entity/User.java
@@ -1,5 +1,74 @@
 package com.portfolio.portofolio.security.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import jakarta.validation.constraints.NotNull;
+
+
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Column(unique = true)
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+    
+    public User(@NotNull String email, @NotNull String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
     
 }
--- src/main/java/com/portfolio/portofolio/security/enums/RoleName.java
+++ src/main/java/com/portfolio/portofolio/security/enums/RoleName.java
@@ -1,5 +1,5 @@
 package com.portfolio.portofolio.security.enums;
 
 public enum RoleName {
-    
+    ROLE_ADMIN, ROLE_USER
 }
