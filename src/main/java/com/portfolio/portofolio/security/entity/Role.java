432ce7715615051754b952658be40b5d9c945204
Agustin Boccio <aboccio@hotmail.com>
2022-08-09 18:47:58 +0200

    Class Role

--- src/main/java/com/portfolio/portofolio/security/entity/Role.java
+++ src/main/java/com/portfolio/portofolio/security/entity/Role.java
@@ -1,5 +1,47 @@
 package com.portfolio.portofolio.security.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.portfolio.portofolio.security.enums.RoleName;
+
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+    
+    public Role() {
+    }
+    
+    public Role(@NotNull RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
     
 }
